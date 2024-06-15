bb1454a287027de8cf780b9c2b997e358d841ce9
Yen Ju Andrew Chiang <37291863+chiangyenju@users.noreply.github.com>
2024-06-15 15:24:44 +0800

    Initial Commit

---
+++ Thrive/ThriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThriveApp.swift
+//  Thrive
+//
+//  Created by Yen Ju Chiang on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ThriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
